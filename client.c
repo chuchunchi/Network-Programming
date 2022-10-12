3d87ce7dc676b3a5a9a6bfc8be4683b007d3febc
chuchunchi <chuchun1231@gmail.com>
2022-10-12 15:32:03 +0800

    can connect server and client

--- client.c
+++ client.c
@@ -7,20 +7,20 @@
 #include <netinet/in.h>
 
 int main(int argc, char *argv[]){
-	int IP = argv[1];
+	char * IP = argv[1];
 	int portnum = atoi(argv[2]);
-	int udpFd = socket(AF_INET,SOCK_DGRAM,0);
-        if(udpFd==-1) printf("socket create fail.");
+	int tcpFd = socket(AF_INET,SOCK_STREAM,0);
+        if(tcpFd==-1) printf("socket create fail.\n");
         struct sockaddr_in info;
         bzero(&info,sizeof(info));
-        info.sin_family = PF_INET;
+        info.sin_family = AF_INET;
         info.sin_addr.s_addr = inet_addr(IP);
         info.sin_port = htons(portnum);
-	int err = connect(sockfd,(struct sockaddr *)&info,sizeof(info));
-	if(err==-1) printf("connect error");
-	char receivemsg[];
-	recv(udfFd,receivemsg,sizeof(receivemsg),0);
-	printf("%s",receivemsg);
+	int err = connect(tcpFd,(struct sockaddr *)&info,sizeof(info));
+	if(err==-1) printf("connect error\n");
+	char receivemsg[100];
+	recv(tcpFd,receivemsg,sizeof(receivemsg),0);
+	printf("%s\n",receivemsg);
 
 		
 }
--- server.c
+++ server.c
@@ -9,20 +9,23 @@
 
 int main(int argc, char *argv[]){
 	int portnum = atoi(argv[1]);
-	int udpFd = socket(AF_INET,SOCK_DGRAM,0);
+	int tcpFd = socket(AF_INET,SOCK_STREAM,0);
 	char* msg = "*****Welcome to Game 1A2B*****";
-	if(udpFd==-1) printf("socket create fail.");
+	if(tcpFd==-1) printf("socket create fail.\n");
 	struct sockaddr_in info,client_info;
 	bzero(&info,sizeof(info));
-	info.sin_family = PF_INET;
+	info.sin_family = AF_INET;
 	info.sin_addr.s_addr = INADDR_ANY;
 	info.sin_port = htons(portnum);
-	bind(udpFd,(struct sockaddr *)&info,sizeof(info));
-	listen(udpFd,5);
-	int new_client = accept(udpFd,(struct sockaddr *) client_info, &sizeof(client_info));
-	if(new_client==-1) printf("connection error");
+	int b = bind(tcpFd,(struct sockaddr *)&info,sizeof(info));
+	if(b==-1) printf("bind error\n");
+	int l = listen(tcpFd,10);
+	if(l==-1) printf("listen error\n");
+	int info_size = sizeof(client_info);
+	int new_client = accept(tcpFd,(struct sockaddr *) &client_info, &info_size);
+	if(new_client==-1) printf("connection error\n");
 	else{
-		printf("New connection.");
+		printf("New connection.\n");
 		send(new_client, msg, strlen(msg), 0); 
 	}	
 }
