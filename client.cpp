49fac0c830367d9402ffb4fbdfaae558b7844a2f
chuchunchi <chuchun1231@gmail.com>
2022-10-13 22:58:29 +0800

    done tcp and udp socket, haven't run yet

--- client.cpp
+++ client.cpp
@@ -6,17 +6,72 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <string>
+#include <cstring>
 using namespace std;
+string reg_usage = "Usage: register <username> <email> <password>";
+string login_usage = "Usage: login <username> <password>";
+string start_usage = "Usage: start-game <4-digit number>";
+string IOHandle(char * recvmsg,int * istcp){
+	string sendback="";
+	vector<string> command;
+	command.push_back("");
+	int para=0;
+	cout << strlen(recvmsg) << endl;
+	for(int i=0;i<strlen(recvmsg);i++){
+		if(recvmsg[i]!=' '){
+			command[para]+=recvmsg[i];
+		}
+		else{
+			command.push_back("");
+			para++;
+		}
+	}
+	if(command[0]=="register"){
+		*istcp = 0;
+		if(command.size()!=4){
+			sendback = reg_usage;
+		}
+	}
+	else if(command[0]=="login"){
+		if(command.size()!=3){
+			sendback = login_usage;
+		}
+	}
+	else if(command[0]=="start-game"){
+		try{stoi(command[1])}
+		catch(...){
+			sendback = start_usage;
+		}
+		if(command.size()!=2) sendback = start_usage;
+	}
+	else if(command[0]=="game-rule"){
+		*istcp = 0;
+	}
+    return sendback;
+}
 int main(int argc, char *argv[]){
 	char * IP = argv[1];
 	int portnum = atoi(argv[2]);
+	// udp socket create
+	int udpFd = socket(AF_INET,SOCK_DGRAM,0);
+    if(udpFd==-1) printf("udp socket create fail.\n");
+    struct sockaddr_in udpinfo;
+    bzero(&info,sizeof(udpinfo));
+    udpinfo.sin_family = AF_INET;
+    udpinfo.sin_addr.s_addr = inet_addr(IP);
+    udpinfo.sin_port = htons(portnum);
+	int erru = connect(udpFd,(struct sockaddr *)&udpinfo,sizeof(udpinfo));
+	if(erru==-1) printf("connect error\n");
+
+	//tcp socket create
 	int tcpFd = socket(AF_INET,SOCK_STREAM,0);
-        if(tcpFd==-1) printf("socket create fail.\n");
-        struct sockaddr_in info;
-        bzero(&info,sizeof(info));
-        info.sin_family = AF_INET;
-        info.sin_addr.s_addr = inet_addr(IP);
-        info.sin_port = htons(portnum);
+    if(tcpFd==-1) printf("socket create fail.\n");
+    struct sockaddr_in info;
+    bzero(&info,sizeof(info));
+    info.sin_family = AF_INET;
+    info.sin_addr.s_addr = inet_addr(IP);
+    info.sin_port = htons(portnum);
 	int err = connect(tcpFd,(struct sockaddr *)&info,sizeof(info));
 	if(err==-1) printf("connect error\n");
 	char receivemsg[1024];
@@ -26,10 +81,25 @@ int main(int argc, char *argv[]){
 	while(1){
 		char command[1024];
 		cin.getline(command,1024);
-		int s=send(tcpFd,command,sizeof(command),0);
-		if(s==-1) cout << "send error\n";
-		char receivemsg[1024];
-		recv(tcpFd,receivemsg,sizeof(receivemsg),0);
-		cout << receivemsg << '\n';
+		int istcp=1;
+		string usage = IOHandle(command,&istcp);
+		if(usage!=""){
+			cout << usage << '\n';
+			continue;
+		}
+		if(istcp){
+			int s=send(tcpFd,command,sizeof(command),0);
+			if(s==-1) cout << "send error\n";
+			char receivemsg[1024];
+			recv(tcpFd,receivemsg,sizeof(receivemsg),0);
+			cout << receivemsg << '\n';
+		}
+		else{
+			int s=send(udpFd,command,sizeof(command),0);
+			if(s==-1) cout << "send error\n";
+			char receivemsg[1024];
+			recv(udpFd,receivemsg,sizeof(receivemsg),0);
+			cout << receivemsg << '\n';
+		}
 	}	
 }
--- server.cpp
+++ server.cpp
@@ -10,13 +10,67 @@
 #include <arpa/inet.h>
 #include <errno.h>
 #include <sys/time.h>
+#include <string>
+#include <cstring>
 using namespace std;
-
-char * IOHandle(char *recvmsg){}
+_register(string username,string email,string password);
+_login(string username,string password);
+_logout();
+_gamerule();
+_startgame(int number);
+_exit();
+char * IOHandle(char *recvmsg){
+	char sendback[1024];
+	vector<string> command;
+	command.push_back("");
+	int para=0;
+	cout << strlen(recvmsg) << endl;
+	for(int i=0;i<strlen(recvmsg);i++){
+		if(recvmsg[i]!=' '){
+			command[para]+=recvmsg[i];
+		}
+		else{
+			command.push_back("");
+			//cout << command[para] << endl;
+			para++;
+		}
+	}
+	if(command[0]=="register"){
+		_register(command[1],command[2],command[3]);
+	}
+	else if(command[0]=="login"){
+		_login(command[1],command[2]);	
+	}
+	else if(command[0]=="logout"){
+		_logout();
+	}
+	else if(command[0]=="game-rule"){
+		_gamerule();
+	}
+	else if(command[0]=="start-game"){
+		_startgame(stoi(command[1]));
+	}
+	else if(command[0]=="exit"){
+		_exit();
+	}
+    return sendback;
+}
 
 int main(int argc, char *argv[]){
 	int portnum = atoi(argv[1]);
 	vector<int> client_sds (10,0);
+
+	//udp socket create
+	int udpFd = socket(AF_INET,SOCK_DGRAM,0);\
+	if(udpFd==-1) printf("socket create fail.\n");
+	int optu;
+	int multiconnectu = setsockopt(udpFd,SOL_SOCKET,SO_REUSEADDR,(char *)&optu,sizeof(optu));
+	if(multiconnectu==-1) printf("set sockopt fail!\n");
+	
+	int bu = bind(udpFd,(struct sockaddr *)&infou,sizeof(infou));
+	if(bu==-1) printf("bind error\n");
+
+	//tcp socket create
 	int tcpFd = socket(AF_INET,SOCK_STREAM,0);
 	char msg[40] = "*****Welcome to Game 1A2B*****";
 	if(tcpFd==-1) printf("socket create fail.\n");
@@ -61,6 +115,13 @@ int main(int argc, char *argv[]){
 					client_sds[i] = new_client;
 					break;
 				}
+			}
+		}
+		if(FD_ISSET(udpFd,&readfds)){
+			char bufu[1024];
+			int r = recv(udpFd,bufu,1024,0);
+			if(r!=0){
+				cout << "recv from udp: " << bufu << '\n';
 			}
 		}
 		//old connection's operation
